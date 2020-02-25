98f00f22ae872556669dc6dd307c795b97315a57
Ankit Kanojia <ankitkanojia.rs@gmail.com>
2020-02-25 13:00:12 +0530

    Added ribbon into the project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Video from 'twilio-video';
 import axios from 'axios';
+import './global.css';
 
 class App extends Component {
     constructor(props) {
@@ -184,6 +185,7 @@ class App extends Component {
         return (
           <React.Fragment>
             <div className="container">
+              <a target="_blank" href="https://github.com/ankitkanojia/twillio_videochat"><img className="githubribbon attachment-full size-full" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_green_007200.png?resize=149%2C149" alt="Fork me on GitHub" data-recalc-dims="1" /></a>
               <h2 className="mt-2">Twillio Prgrammable Video Chat</h2>
               {!this.state.hasJoinedRoom &&
                 <div className="row">
